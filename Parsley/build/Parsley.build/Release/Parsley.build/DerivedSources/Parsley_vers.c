0c0ac9d728957bb30a1f06f2268086da02d53a38
Jaden Geller <jaden.geller@gmail.com>
2016-01-12 17:25:41 -0800

    Added docs directory

---
+++ Parsley/build/Parsley.build/Release/Parsley.build/DerivedSources/Parsley_vers.c
@@ -0,0 +1,1 @@
+ const unsigned char ParsleyVersionString[] __attribute__ ((used)) = "@(#)PROGRAM:Parsley  PROJECT:Parsley-1" "\n"; const double ParsleyVersionNumber __attribute__ ((used)) = (double)1.;
